03eacdd012ea0097e49125bdc39ea682c47e5b0d
SaraBolada157 <161457311+SaraBolada157@users.noreply.github.com>
2024-06-14 08:02:30 -0300

    oinç

--- main.js
+++ main.js
@@ -18,40 +18,6 @@ function calculaTempo(tempoObjetivo) {
     let minutos = Math.floor(segundos / 60);
     let horas = Math.floor(minutos / 60);
     let dias = Math.floor(horas / 24);
-    return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-    segundos %= 60;
-    minutos %= 60;
-    horas %= 24;
-
-    const tempoObjetivo2 = new Date("2023-12-05T00:00:00");
-const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
-const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
-contadores[0].textContent = calculaTempo(tempoObjetivo1); //Antes
-
-contadores[0].textContent = calculaTempo(tempos[0]); // Depois
-// Para cada objetivo na lista de contadores
-for (let i=0; i<contadores.length;i++){
-    //Calcular o tempo usando a função e associá-lo ao objetivo
-            contadores[i].textContent = calculaTempo(tempos[i]);   
-    // Para cada objetivo na lista de contadores
- for (let i=0; i<contadores.length;i++){
-    //Calcular o tempo usando a função e associá-lo ao objetivo
-            contadores[i].textContent = calculaTempo(tempos[i]);   
+   
     
-            function atualizaCronometro(){
-                for (let i=0; i<contadores.length;i++){
-                    contadores[i].textContent = calculaTempo(tempos[i]);   
-                }
-            }
-            function comecaCronometro(){
-                atualizaCronometro();
-                setInterval(atualizaCronometro,1000);
-            }
-            
-            comecaCronometro();
-            if (tempoFinal > 0){
-                return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos";
-            } else {
-                return "Prazo Finalizado";
-            }
         
